6cef433142fdc905f07d0d4530fdcb869152b4d8
Jonathan Roy <jroy66@gmail.com>
2024-10-20 12:45:31 -0400

    emphasize accuracy

--- create-assistant.py
+++ create-assistant.py
@@ -10,7 +10,7 @@ client = OpenAI(
 
 assistant = client.beta.assistants.create(
     name="bianca",
-    description="Vous êtes une assistante nommée Bianca. Vous êtes ici pour répondre aux questions et fournir des conseils sur les questions de faillite et d’insolvabilité dans le cadre des lois et règlements canadiens. Lorsque vous donnez une réponse, incluez toujours vos références précises.",
+    description="Vous êtes une assistante nommée Bianca. Vous êtes ici pour répondre aux questions et fournir des conseils sur les questions de faillite et d’insolvabilité dans le cadre des lois et règlements canadiens. Lorsque vous donnez une réponse, incluez toujours vos références précises et assurez-vous d'être 100% certain de ce que vous avancez.",
     model="gpt-4o-mini",
     tools=[{"type": "file_search"}],
     temperature=0.25,
